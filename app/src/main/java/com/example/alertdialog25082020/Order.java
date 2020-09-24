08b15c29a0526c4524c9fd6bdaaa14b648ffce76
phatdroid94 <phatdroid94@gmail.com>
2020-09-24 21:14:43 +0700

    builder

--- app/src/main/java/com/example/alertdialog25082020/Order.java
+++ app/src/main/java/com/example/alertdialog25082020/Order.java
@@ -45,4 +45,40 @@ public class Order {
     public void setVegetableType(VegetableType vegetableType) {
         this.vegetableType = vegetableType;
     }
+
+    class Builder implements OrderBuilder{
+        private Location location;
+        private BreadType breadType;
+        private SauceType sauceType;
+        private VegetableType vegetableType;
+        @Override
+        public OrderBuilder setLocation(Location location) {
+            this.location = location;
+            return this;
+        }
+
+        @Override
+        public OrderBuilder setBreadType(BreadType breadType) {
+            this.breadType = breadType;
+            return this;
+        }
+
+        @Override
+        public OrderBuilder setSauceType(SauceType sauceType) {
+            this.sauceType = sauceType;
+            return this;
+        }
+
+        @Override
+        public OrderBuilder setVegetableType(VegetableType vegetableType) {
+            this.vegetableType = vegetableType;
+            return this;
+        }
+
+        @Override
+        public Order build() {
+            return new Order(location,breadType,sauceType,vegetableType);
+        }
+    }
+
 }
