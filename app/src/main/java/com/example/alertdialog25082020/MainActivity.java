8a8dad9de45247c6cdcc01575d28ebefd1d02b87
phatdroid94 <phatdroid94@gmail.com>
2020-09-26 20:07:00 +0700

    gan du lieu cho alert

--- app/src/main/java/com/example/alertdialog25082020/MainActivity.java
+++ app/src/main/java/com/example/alertdialog25082020/MainActivity.java
@@ -2,22 +2,67 @@ package com.example.alertdialog25082020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button mBtnOpen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Order order = new Order.Builder()
-                .setBreadType(BreadType.BEEF)
-                .setLocation(Location.TAKE_AWAY)
-                .setVegetableType(VegetableType.TOMATO)
-                .build();
-        Log.d("BBB", order.toString());
+//        Order order = new Order.Builder()
+//                .setBreadType(BreadType.BEEF)
+//                .setLocation(Location.TAKE_AWAY)
+//                .setVegetableType(VegetableType.TOMATO)
+//                .build();
+//        Log.d("BBB", order.toString());
+        mBtnOpen = findViewById(R.id.buttonOpen);
+
+        mBtnOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Thông báo");
+                builder.setMessage("Bạn có muốn thoát ứng dụng");
+                builder.setIcon(R.mipmap.ic_launcher);
+                builder.setCancelable(false);
+
+                // Nút có
+                builder.setPositiveButton("Có", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+                // Nút không
+                builder.setNegativeButton("Không", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+                // Nút hủy
+                builder.setNeutralButton("Hủy", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+                builder.show();
+
+
+            }
+        });
     }
 
 }
