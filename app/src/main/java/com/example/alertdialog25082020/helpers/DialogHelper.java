a99651ac6770266f9012ee68bb8fae07049a43ac
phatdroid94 <phatdroid94@gmail.com>
2020-09-26 21:24:23 +0700

    gan layout cho dialog

--- app/src/main/java/com/example/alertdialog25082020/helpers/DialogHelper.java
+++ app/src/main/java/com/example/alertdialog25082020/helpers/DialogHelper.java
@@ -2,12 +2,18 @@ package com.example.alertdialog25082020.helpers;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.example.alertdialog25082020.R;
 
 public class DialogHelper {
 
     public void showLoginDialog(Context context){
         Dialog dialog = new Dialog(context, android.R.style.Theme_DeviceDefault_Dialog_Alert);
-
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        View v = layoutInflater.inflate(R.layout.dialog_login,null,false);
+        dialog.setContentView(v);
     }
 }
 
